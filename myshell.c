89b26ae831f741a18307df8b364630e23441394c
mth18 <mth18@students.uwf.edu>
2014-09-12 14:17:49 -0500

    Remake of myshell.c to test fork

--- myshell.c
+++ myshell.c
@@ -1,14 +1,31 @@
+#include "Parse.h"
+#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include "myshell.h"
 
-void parsestr(char *cmd)
+int main()
 {
-	char *token = strtok(cmd," \t\n");
-	while (token != NULL)
-	{
-		
-		printf ("%s\n",token);
-		token = strtok(NULL, " \t\n");
-	}
+    forkingtest();
+    return 0;
+}
 
+void forkingtest()
+{
+     pid_t p; //predefined system type int
+     int status; //use for wait pid
+     p = fork();
+     printf("%i child process number\n",p);
+     if(p>0)
+     {
+            printf("Parent Process\n");
+     }
+     else
+     {
+         printf("Child Process\n");
+     }
 }
